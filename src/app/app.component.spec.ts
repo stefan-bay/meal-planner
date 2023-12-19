8ce4ed74d316747de5a1bf2b94bd1a506ec16019
stefan-bay <stefanbay1@gmail.com>
2023-12-18 21:28:33 -0500

    Add eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'meal-planner' title`, () => {
+  it('should have the \'meal-planner\' title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('meal-planner');
