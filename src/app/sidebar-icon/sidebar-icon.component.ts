1154bf01e30c94eb482129400c06a6e78274a877
stefan-bay <stefanbay1@gmail.com>
2023-12-19 23:19:08 -0500

    cleanup sidebar-icon template

--- src/app/sidebar-icon/sidebar-icon.component.ts
+++ src/app/sidebar-icon/sidebar-icon.component.ts
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
     imports: [],
     template: `<div
         class="
-        relative m-2
-        flex h-14
-        w-14
-        items-center justify-center rounded-md bg-gray-500 text-white shadow-lg
+        relative m-2 flex h-14 w-14
+        items-center justify-center
+        rounded-md bg-gray-500 text-white shadow-lg
         transition-all hover:bg-gray-600
         md:h-12 md:w-12
         "
