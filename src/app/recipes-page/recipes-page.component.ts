e41f3eea8c6e7dc8e66bf84b2e7e64fbf84fc9fd
stefan-bay <stefanbay1@gmail.com>
2023-12-22 13:33:02 -0500

    initial recipe work

--- src/app/recipes-page/recipes-page.component.ts
+++ src/app/recipes-page/recipes-page.component.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
     imports: [],
     templateUrl: './recipes-page.component.html',
 })
-export class RecipesPageComponent {}
+export class RecipesPageComponent {
+    rec = {
+        name: 'Beer Brats',
+        instructions:
+            'Cover brats, chopped onion, and butter with beer. Bring to a boil, simmer on medium for 10-15 mins',
+        items: [
+            {
+                name: 'Bratwurst',
+                quantity: '1',
+                unit: 'lb',
+                category: 'Meat',
+            },
+            {
+                name: 'Onion',
+                quantity: '1',
+                unit: '',
+                category: 'Meat',
+            },
+            {
+                name: 'Butter',
+                quantity: '4',
+                unit: 'tbsp',
+                category: 'Dairy',
+            },
+            {
+                name: 'Beers',
+                quantity: '2',
+                unit: '',
+                category: 'Alcohol',
+            },
+        ],
+    };
+}
