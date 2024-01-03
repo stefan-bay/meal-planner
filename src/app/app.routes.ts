e721852348427da13cc25a0f18a3409b8842aafc
Stefan Bay <stefanbay@beitzelcs.com>
2024-01-02 20:21:14 -0500

    add login and signup components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { type Routes } from '@angular/router';
 
+import { LoginComponent } from './components/authentication/login/login.component';
+import { SignupComponent } from './components/authentication/signup/signup.component';
 import { ListRecipesComponent } from './components/recipes-page/list-recipes/list-recipes.component';
 import { ViewRecipeComponent } from './components/recipes-page/view-recipe/view-recipe.component';
 import { EditRecipeComponent } from './components/recipes-page/edit-recipe/edit-recipe.component';
@@ -10,6 +12,14 @@ export const routes: Routes = [
         path: '',
         pathMatch: 'full',
         redirectTo: '/home',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'signup',
+        component: SignupComponent,
     },
     {
         path: 'recipes',
