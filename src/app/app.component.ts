4c96bf79136056e3a783e97b1b33b065411fc2a3
Stefan Bay <stefanbay@beitzelcs.com>
2024-02-15 20:30:10 -0500

    remove comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,6 @@ export class AppComponent {
 
     constructor() {
         effect(() => {
-            // undefined = authentication yet to be checked
-            // null = unauthenticated
             if (this.authService.user() === null) {
                 const signingUp = this.router.url === '/signup';
                 if (signingUp) {
